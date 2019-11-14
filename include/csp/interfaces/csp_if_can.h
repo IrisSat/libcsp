183059dc1b0ebec1675cf581a9fb635bbf0d8b0c
Joseph <josephhowarth17@gmail.com>
2019-11-14 11:43:54 -0600

    Added function to get the rx queue from can interface

--- include/csp/interfaces/csp_if_can.h
+++ include/csp/interfaces/csp_if_can.h
@@ -51,6 +51,8 @@ struct csp_can_config {
  */
 int csp_can_init(uint8_t mode, struct csp_can_config *conf);
 
+csp_queue_handle_t * get_csp_can_queue();
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
